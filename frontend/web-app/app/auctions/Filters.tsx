8347d1ec5ef38505e3c6c3b9d5f4ad027be96181
Nasir Iqbal <nasirpucit@hotmail.com>
2023-09-20 08:17:27 +0100

    96. Refactored code to use the zustand state

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,5 +1,5 @@
+import { useParamsStore } from '@/hooks/useParamsStore';
 import { Button } from 'flowbite-react';
-import ButtonGroup from 'flowbite-react/lib/esm/components/Button/ButtonGroup';
 import React from 'react'
 
 type Props = {
@@ -9,23 +9,27 @@ type Props = {
 
 const pageSizeButtons = [4, 8, 12];
 
-export default function Filters({pageSize, setPageSize}: Props) {
-  return (
-    <div className='flex justify-between items-center mb-4'>
-        <div>
-            <span className='uppercase text-sm text-gray-500 mr-2'>Page size</span>
-            <ButtonGroup>
-                {pageSizeButtons.map((value, i) => (
-                   <Button key={i} 
-                        onClick={() => setPageSize(value)}
-                        color={`${pageSize === value ? 'red' : 'gray'}`}
-                        className='focus:ring-0'
-                    >
-                        {value}
-                   </Button> 
-                ))}
-            </ButtonGroup>
+export default function Filters() {
+    const pageSize = useParamsStore(state => state.pageSize);
+    const setParams = useParamsStore(state => state.setParams);
+
+
+    return (
+        <div className='flex justify-between items-center mb-4'>
+            <div>
+                <span className='uppercase text-sm text-gray-500 mr-2'>Page size</span>
+                <Button.Group>
+                    {pageSizeButtons.map((value, i) => (
+                        <Button key={i}
+                            onClick={() => setParams({ pageSize: value })}
+                            color={`${pageSize === value ? 'red' : 'gray'}`}
+                            className='focus:ring-0'
+                        >
+                            {value}
+                        </Button>
+                    ))}
+                </Button.Group>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
--- frontend/web-app/hooks/useParamsStore.ts
+++ frontend/web-app/hooks/useParamsStore.ts
@@ -20,17 +20,17 @@ const initialState: State = {
 }
 
 export const useParamsStore = create<State & Actions>()((set) => ({
-   ...initialState,
+    ...initialState,
 
-   setParams: (newParams: Partial<State>) => {
-    set((state) => {
-        if (newParams.pageNumber) {
-            return {...state, pageNumber: newParams.pageNumber}
-        } else {
-            return {...state, ...newParams, pageNumber: 1}
-        }
-    })
-   },
+    setParams: (newParams: Partial<State>) => {
+        set((state) => {
+            if (newParams.pageNumber) {
+                return { ...state, pageNumber: newParams.pageNumber }
+            } else {
+                return { ...state, ...newParams, pageNumber: 1 }
+            }
+        })
+    },
 
-   reset: () => set(initialState)
+    reset: () => set(initialState)
 }))
