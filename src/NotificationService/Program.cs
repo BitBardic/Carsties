c02b47b751c5424d1a6e93c1bf73633571f6bcd2
Nasir Iqbal <nasirpucit@hotmail.com>
2023-11-03 22:54:59 +0000

    150. Added a SignalR Hub

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -1,10 +1,10 @@
 using MassTransit;
+using NotificationService.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMassTransit(x =>
 {
-
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
 
     x.UsingRabbitMq((context, cfg) =>
@@ -18,8 +18,10 @@ builder.Services.AddMassTransit(x =>
         cfg.ConfigureEndpoints(context);
     });
 });
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+app.MapHub<NotificationHub>("/notifications");
 
 app.Run();
