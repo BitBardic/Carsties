794e14300b4b565799597cb60d6dd5b9c62fa1b6
Nasir Iqbal <nasirpucit@hotmail.com>
2023-11-04 19:06:57 +0000

    158. Updated the Auction cards with the current high price

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -3,6 +3,7 @@ import CountdownTimer from './CountdownTimer'
 import CartImage from './CartImage'
 import { Auction } from '@/types'
 import Link from 'next/link'
+import CurrentBid from './CurrentBid'
 
 type Props = {
     auction: Auction
@@ -16,6 +17,11 @@ export default function AuctionCard({ auction }: Props) {
                     <CartImage imageUrl={auction.imageUrl} />
                     <div className='absolute bottom-2 left-2'>
                         <CountdownTimer auctionEnd={auction.auctionEnd} />
+                    </div>
+                    <div className='absolute top-2 right-2'>
+                        <CurrentBid 
+                            reservedPrice={auction.reservePrice} 
+                            amount={auction.currentHighBid} />
                     </div>
                 </div>
             </div>
