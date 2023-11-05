c020ceb9d9ccc4486562fbfbff66a2d8289d8ff7
Nasir Iqbal <nasirpucit@hotmail.com>
2023-11-05 17:25:11 +0000

    166. Added the new bid to SignalR

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -31,10 +31,10 @@ export default function SignalRProvider({children}: Props) {
                     console.log('Connected to notification hub');
 
                     connection.on('BidPlaced', (bid: Bid) => {
-                        console.log('Bid placed event received');
                         if (bid.bidStatus.includes('Accepted')) {
                             setCurrentPrice(bid.auctionId, bid.amount)
                         }
+                        addBid(bid);
                     })
                 }).catch(error => console.log(error));
         }
@@ -42,7 +42,7 @@ export default function SignalRProvider({children}: Props) {
         return () => {
             connection?.stop();
         }
-    }, [connection, setCurrentPrice])
+    }, [connection, setCurrentPrice, addBid])
 
     return (
         children
